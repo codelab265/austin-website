666b064a9426687d4eb202387f59f0052930063b
codelab265 <mphatsomlenga1@gmail.com>
2025-09-27 09:18:37 +0200

    Update asset filenames in manifest.json, remove obsolete files, and add new components for improved functionality and organization

---
+++ public/build/assets/Testimonies-vWnqKFK0.js
@@ -0,0 +1,11 @@
+import{j as e}from"./app-B6cziFBO.js";import{c as t}from"./MainLayout-DI6flD8m.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],i=t("ChevronLeft",s);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],a=t("ChevronRight",n);function l(){return e.jsx("div",{className:"w-full flex items-center justify-center gap-[43px]",children:e.jsxs("div",{className:"w-full max-w-[1130px] px-[65px] py-[55px] bg-[#EA413C]",children:[e.jsx("div",{className:"text-[32px] font-medium text-white/50 -tracking-[2%] leading-[30px] uppercase",children:"Malawi"}),e.jsx("div",{className:"text-2xl font-aeonik text-white tracking-normal mt-[37px]",children:"Of a truth, I went on a retreat with the book; The Field of Boaz. the lines of the book carries great prayer points as i needed to read and also write. My friends had gotten their copies too. The book is addicting. Thank you sir for writing this."}),e.jsxs("div",{className:"flex items-center justify-between mt-[88px]",children:[e.jsx("div",{className:"font-medium text-white text-[48px] leading-[30px] -tracking-[2%] uppercase ",children:"Mphatso Mlenga"}),e.jsxs("div",{className:"flex items-center gap-[10px]",children:[e.jsx("div",{className:"size-[65px] rounded-full flex items-center justify-center bg-black",children:e.jsx(i,{className:"text-white"})}),e.jsx("div",{className:"size-[65px] rounded-full flex items-center justify-center bg-black",children:e.jsx(a,{className:"text-white"})})]})]})]})})}function c(){return e.jsxs("section",{className:"bg-white flex flex-col items-center w-full",children:[e.jsx("h1",{className:"text-[#070707] text-[64px] sm:text-[100px] md:text-[150px] lg:text-[200px] font-semibold -tracking-[2%] xl:leading-[150px] mb-[66px]",children:"TESTIMONIES"}),e.jsx("div",{className:"w-full",children:e.jsx(l,{})})]})}export{c as T};
